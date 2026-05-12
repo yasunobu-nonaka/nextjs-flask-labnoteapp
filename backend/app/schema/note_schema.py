8ab9b56d8be6691a9bfb5fa09512f47252b9b9cd
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-12 15:16:43 +0900

    Add validations in NoteSchema

--- backend/app/schema/note_schema.py
+++ backend/app/schema/note_schema.py
@@ -1,10 +1,10 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class NoteSchema(Schema):
-    id = fields.Int()
-    user_id = fields.Int()
-    title = fields.Str()
-    content_md = fields.Str()
-    created_at = fields.DateTime()
-    updated_at = fields.DateTime()
+    id = fields.Int(dump_only=True)
+    user_id = fields.Int(dump_only=True)
+    title = fields.Str(required=True, validate=validate.Length(min=1, max=200))
+    content_md = fields.Str(required=True, validate=validate.Length(min=1))
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
