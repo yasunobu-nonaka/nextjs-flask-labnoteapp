1cf438d9d92aefc53acb278725689be9f5aedb53
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-14 11:01:25 +0900

    Update user query in user_lookup_loader to follow SQLAlchemy 2.X

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -23,7 +23,7 @@ def create_app(config_name="development"):
     @jwt.user_lookup_loader
     def user_lookup_callback(_jwt_header, jwt_data):
         identity = jwt_data["sub"]
-        return User.query.filter_by(id=identity).one_or_none()
+        return db.session.get(User, identity)
 
     app.register_blueprint(api_bp)
 
