51fb5beaeecfdeac22443792f1de7d8a0ff583de
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-15 00:17:45 +0900

    Add tests for login validation.

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -112,3 +112,60 @@ def test_too_long_password_register_failed(client):
         == "パスワードは12文字以上64字以下にしてください"
     )
     assert res.status_code == 400
+
+
+#############################################
+# tests for login
+#############################################
+
+
+def test_login(client):
+    client.post(
+        "/api/auth/register",
+        json={
+            "username": "testuser",
+            "password": "testuser1234",
+            "confirm": "testuser1234",
+        },
+    )
+
+    res = client.post(
+        "/api/auth/login", json={"username": "testuser", "password": "testuser1234"}
+    )
+
+    assert "access_token" in res.json
+    assert res.status_code == 200
+
+
+def test_no_username_login_failed(client):
+    client.post(
+        "/api/auth/register",
+        json={
+            "username": "testuser",
+            "password": "testuser1234",
+            "confirm": "testuser1234",
+        },
+    )
+
+    res = client.post("/api/auth/login", json={"password": "testuser1234"})
+
+    assert "access_token" not in res.json
+    assert res.json["errors"]["username"][0] == "Missing data for required field."
+    assert res.status_code == 400
+
+
+def test_no_password_login_failed(client):
+    client.post(
+        "/api/auth/register",
+        json={
+            "username": "testuser",
+            "password": "testuser1234",
+            "confirm": "testuser1234",
+        },
+    )
+
+    res = client.post("/api/auth/login", json={"username": "testuser"})
+
+    assert "access_token" not in res.json
+    assert res.json["errors"]["password"][0] == "Missing data for required field."
+    assert res.status_code == 400
