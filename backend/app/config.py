e37f24f16ad9abebdf94609bbf00c30ca12eeb4a
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-14 19:21:48 +0900

    Change database for testing from memory to file database.

--- backend/app/config.py
+++ backend/app/config.py
@@ -19,7 +19,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
 
 
 class ProductionConfig(Config):
