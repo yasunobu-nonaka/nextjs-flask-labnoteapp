a51df63d467e3b0fa4bc02a8ee55d9fa8a35bea3
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-15 14:56:39 +0900

    Add test for create_note(), mostly checking NoteCreationSchema works correctly.

--- backend/tests/test_notes.py
+++ backend/tests/test_notes.py
@@ -1,3 +1,8 @@
+#############################################
+# tests for note creation
+#############################################
+
+
 def test_note_creation(client, auth_headers):
     res = client.post(
         "/api/notes",
@@ -7,3 +12,65 @@ def test_note_creation(client, auth_headers):
 
     assert res.get_json()["message"] == "Note created successfully!"
     assert res.status_code == 201
+
+
+def test_no_title_note_creation_failed(client, auth_headers):
+    res = client.post(
+        "/api/notes",
+        json={"content_md": "Note Content Here!"},
+        headers=auth_headers,
+    )
+
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["title"][0] == "Missing data for required field."
+    assert res.status_code == 400
+
+
+def test_no_content_note_creation_failed(client, auth_headers):
+    res = client.post(
+        "/api/notes",
+        json={"title": "Note Title"},
+        headers=auth_headers,
+    )
+
+    assert res.get_json()["message"] == "validation error"
+    assert (
+        res.get_json()["errors"]["content_md"][0] == "Missing data for required field."
+    )
+    assert res.status_code == 400
+
+
+def test_empty_title_note_creation_failed(client, auth_headers):
+    res = client.post(
+        "/api/notes",
+        json={"title": "", "content_md": "Note Content Here!"},
+        headers=auth_headers,
+    )
+
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["title"][0] == "Length must be between 1 and 200."
+    assert res.status_code == 400
+
+
+def test_empty_content_note_creation_failed(client, auth_headers):
+    res = client.post(
+        "/api/notes",
+        json={"title": "Note Title", "content_md": ""},
+        headers=auth_headers,
+    )
+
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["content_md"][0] == "Shorter than minimum length 1."
+    assert res.status_code == 400
+
+
+def test_too_long_title_note_creation_failed(client, auth_headers):
+    res = client.post(
+        "/api/notes",
+        json={"title": "note title" * 20 + "x", "content_md": "Note Content Here!"},
+        headers=auth_headers,
+    )
+
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["title"][0] == "Length must be between 1 and 200."
+    assert res.status_code == 400
