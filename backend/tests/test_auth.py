bf7f24dbb58e4aca26f4318089a73fe5b7ae5a7d
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-15 13:08:15 +0900

    Update res.json to res.get_json().

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -12,8 +12,8 @@ def test_register(client):
             "confirm": "testuser1234",
         },
     )
-    assert res.json["message"] == "User registration success"
-    assert res.json["username"] == "testuser"
+    assert res.get_json()["message"] == "User registration success"
+    assert res.get_json()["username"] == "testuser"
     assert res.status_code == 200
 
 
@@ -25,8 +25,8 @@ def test_no_username_register_failed(client):
             "confirm": "testuser1234",
         },
     )
-    assert res.json["message"] == "validation error"
-    assert res.json["errors"]["username"][0] == "Missing data for required field."
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["username"][0] == "Missing data for required field."
     assert res.status_code == 400
 
 
@@ -38,8 +38,8 @@ def test_no_confirm_register_failed(client):
             "password": "testuser1234",
         },
     )
-    assert res.json["message"] == "validation error"
-    assert res.json["errors"]["confirm"][0] == "Missing data for required field."
+    assert res.get_json()["message"] == "validation error"
+    assert res.get_json()["errors"]["confirm"][0] == "Missing data for required field."
     assert res.status_code == 400
 
 
@@ -52,9 +52,9 @@ def test_too_short_username_register_failed(client):
             "confirm": "testuser1234",
         },
     )
-    assert res.json["message"] == "validation error"
+    assert res.get_json()["message"] == "validation error"
     assert (
-        res.json["errors"]["username"][0]
+        res.get_json()["errors"]["username"][0]
         == "ユーザー名は4文字以上100字以下にしてください"
     )
     assert res.status_code == 400
@@ -69,9 +69,9 @@ def test_too_long_username_register_failed(client):
             "confirm": "testuser1234",
         },
     )
-    assert res.json["message"] == "validation error"
+    assert res.get_json()["message"] == "validation error"
     assert (
-        res.json["errors"]["username"][0]
+        res.get_json()["errors"]["username"][0]
         == "ユーザー名は4文字以上100字以下にしてください"
     )
     assert res.status_code == 400
@@ -86,9 +86,9 @@ def test_too_short_password_register_failed(client):
             "confirm": "testuser123",
         },
     )
-    assert res.json["message"] == "validation error"
+    assert res.get_json()["message"] == "validation error"
     assert (
-        res.json["errors"]["password"][0]
+        res.get_json()["errors"]["password"][0]
         == "パスワードは12文字以上64字以下にしてください"
     )
     assert res.status_code == 400
@@ -103,9 +103,9 @@ def test_too_long_password_register_failed(client):
             "confirm": "testuser" * 8 + "1",
         },
     )
-    assert res.json["message"] == "validation error"
+    assert res.get_json()["message"] == "validation error"
     assert (
-        res.json["errors"]["password"][0]
+        res.get_json()["errors"]["password"][0]
         == "パスワードは12文字以上64字以下にしてください"
     )
     assert res.status_code == 400
@@ -130,7 +130,7 @@ def test_login(client):
         "/api/auth/login", json={"username": "testuser", "password": "testuser1234"}
     )
 
-    assert "access_token" in res.json
+    assert "access_token" in res.get_json()
     assert res.status_code == 200
 
 
@@ -146,8 +146,8 @@ def test_no_username_login_failed(client):
 
     res = client.post("/api/auth/login", json={"password": "testuser1234"})
 
-    assert "access_token" not in res.json
-    assert res.json["errors"]["username"][0] == "Missing data for required field."
+    assert "access_token" not in res.get_json()
+    assert res.get_json()["errors"]["username"][0] == "Missing data for required field."
     assert res.status_code == 400
 
 
@@ -163,6 +163,6 @@ def test_no_password_login_failed(client):
 
     res = client.post("/api/auth/login", json={"username": "testuser"})
 
-    assert "access_token" not in res.json
-    assert res.json["errors"]["password"][0] == "Missing data for required field."
+    assert "access_token" not in res.get_json()
+    assert res.get_json()["errors"]["password"][0] == "Missing data for required field."
     assert res.status_code == 400
