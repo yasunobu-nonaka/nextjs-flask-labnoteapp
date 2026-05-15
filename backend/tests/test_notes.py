248ac86b94ec1439f233df60226d98887b32506e
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-05-15 21:35:49 +0900

    Add helper functions for note creation, replace existing note creation processes. Fix the bug in test_note_ownership_in_note_index().

--- backend/tests/test_notes.py
+++ backend/tests/test_notes.py
@@ -2,17 +2,21 @@ from app.extensions import db
 from app.model import User
 from conftest import register_user, login_and_get_token
 
+
+def create_note(client, auth_headers, title, content_md):
+    res = client.post(
+        "/api/notes",
+        json={"title": title, "content_md": content_md},
+        headers=auth_headers["headers"],
+    )
+    return res
+
+
 #############################################
 # tests for note creation
 #############################################
-
-
 def test_note_creation(client, auth_headers):
-    res = client.post(
-        "/api/notes",
-        json={"title": "My Note", "content_md": "Note Content Here!"},
-        headers=auth_headers["headers"],
-    )
+    res = create_note(client, auth_headers, "My Note", "Note Content Here!")
 
     assert res.get_json()["message"] == "Note created successfully!"
     assert res.status_code == 201
@@ -45,11 +49,7 @@ def test_no_content_note_creation_failed(client, auth_headers):
 
 
 def test_empty_title_note_creation_failed(client, auth_headers):
-    res = client.post(
-        "/api/notes",
-        json={"title": "", "content_md": "Note Content Here!"},
-        headers=auth_headers["headers"],
-    )
+    res = create_note(client, auth_headers, "", "Note Content Here!")
 
     assert res.get_json()["message"] == "validation error"
     assert res.get_json()["errors"]["title"][0] == "Length must be between 1 and 200."
@@ -57,11 +57,7 @@ def test_empty_title_note_creation_failed(client, auth_headers):
 
 
 def test_empty_content_note_creation_failed(client, auth_headers):
-    res = client.post(
-        "/api/notes",
-        json={"title": "Note Title", "content_md": ""},
-        headers=auth_headers["headers"],
-    )
+    res = create_note(client, auth_headers, "My Note", "")
 
     assert res.get_json()["message"] == "validation error"
     assert res.get_json()["errors"]["content_md"][0] == "Shorter than minimum length 1."
@@ -69,10 +65,8 @@ def test_empty_content_note_creation_failed(client, auth_headers):
 
 
 def test_too_long_title_note_creation_failed(client, auth_headers):
-    res = client.post(
-        "/api/notes",
-        json={"title": "note title" * 20 + "x", "content_md": "Note Content Here!"},
-        headers=auth_headers["headers"],
+    res = create_note(
+        client, auth_headers, "note title" * 20 + "x", "Note Content Here!"
     )
 
     assert res.get_json()["message"] == "validation error"
@@ -84,11 +78,7 @@ def test_too_long_title_note_creation_failed(client, auth_headers):
 # tests for note index
 #############################################
 def test_note_index(client, auth_headers):
-    client.post(
-        "/api/notes",
-        json={"title": "My Note", "content_md": "Note Content Here!"},
-        headers=auth_headers["headers"],
-    )
+    create_note(client, auth_headers, "My Note", "Note Content Here!")
 
     res = client.get("/api/notes", headers=auth_headers["headers"])
 
@@ -119,40 +109,47 @@ def test_note_ownership_in_note_index(client, auth_headers):
 
     # get 2nd uesr's id
     second_user_id = (
-        db.session.execute(db.select(User).filter_by(username="testuser"))
+        db.session.execute(db.select(User).filter_by(username="seconduser"))
         .scalar_one_or_none()
         .id
     )
 
     # create note by testuser and 2nd user
-    client.post(
-        "/api/notes",
-        json={
-            "title": "Testuser's Note",
-            "content_md": "Testuser's Note Content Here!",
-        },
-        headers=auth_headers["headers"],
+    create_note(
+        client, auth_headers, "Testuser's Note", "Testuser's Note Content Here!"
     )
 
+    header_2nduser = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token_2nd_user}",
+    }
     client.post(
         "/api/notes",
         json={
             "title": "2nd User's Note",
             "content_md": "2nd User's Note Content Here!",
         },
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {token_2nd_user}",
-        },
+        headers=header_2nduser,
     )
 
     # get note index as testuser
-    res = client.get("/api/notes", headers=auth_headers["headers"])
+    res_testuser = client.get("/api/notes", headers=auth_headers["headers"])
 
-    assert res.status_code == 200
+    assert res_testuser.status_code == 200
 
-    notes = res.get_json()
+    notes = res_testuser.get_json()
     for note in notes:
         assert note["user_id"] == auth_headers["user_id"]
         assert note["title"] == "Testuser's Note"
         assert note["content_md"] == "Testuser's Note Content Here!"
+
+    # get note index as 2nd user
+    res_2nduser = client.get("/api/notes", headers=header_2nduser)
+
+    assert res_2nduser.status_code == 200
+
+    notes = res_2nduser.get_json()
+    for note in notes:
+        assert note["user_id"] == second_user_id
+        assert note["title"] == "2nd User's Note"
+        assert note["content_md"] == "2nd User's Note Content Here!"
